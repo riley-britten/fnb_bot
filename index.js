fc83fb4d2a5a3c3637d413b8f46f96f36cb813e9
Caoimhe <riley.britten.ph.d@gmail.com>
2023-01-11 21:10:06 -0700

    Add error handling

--- index.js
+++ index.js
@@ -52,26 +52,33 @@ async function main() {
 }
 
 async function displayPeriodicUpdate() {
-  let responseBody = `Reservations for next week:\n`;
-  let haveDistro = false;
-  let haveCooking = false;
-  for (const r of data.reservations) {
-    const timeAfterNow = new Date(r.date).getTime() - new Date().getTime();
-    if (timeAfterNow > 0 && timeAfterNow < 7 * 24 * 60 * 60 * 1000) {
-      responseBody += `${r.user}: ${r.type} on ${new Date(r.date).toDateString()}\n`;
-      if (r.type === 'cooking') {
-        haveCooking = true;
-      } else if (r.type === 'distro') {
-        haveDistro = true;
+  try {
+    let responseBody = `Reservations for next week:\n`;
+    let haveDistro = false;
+    let haveCooking = false;
+    for (const r of data.reservations) {
+      const timeAfterNow = new Date(r.date).getTime() - new Date().getTime();
+      if (timeAfterNow > 0 && timeAfterNow < 7 * 24 * 60 * 60 * 1000) {
+        responseBody += `${r.user}: ${r.type} on ${new Date(r.date).toDateString()}\n`;
+        if (r.type === 'cooking') {
+          haveCooking = true;
+        } else if (r.type === 'distro') {
+          haveDistro = true;
+        }
       }
     }
-  }
-  await bot.chat.send(postChannel, {
-    body: responseBody,
-  });
-  if (!haveCooking || !haveDistro) {
     await bot.chat.send(postChannel, {
-      body: `We could still use volunteers for next week. Please post in ${reserveChannelName} to volunteer.`
+      body: responseBody,
+    });
+    if (!haveCooking || !haveDistro) {
+      await bot.chat.send(postChannel, {
+        body: `We could still use volunteers for next week. Please post in ${reserveChannelName} to volunteer.`
+      })
+    }
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(postChannel, {
+      body: `Failed to display update, see logs for details.`
     })
   }
 }
@@ -83,166 +90,215 @@ async function distroShowOfHands() {
 }
 
 async function displaySchedule(conversationId) {
-  let responseBody = `Reservations:\n`
-  for (const r of data.reservations) {
-    responseBody += `${r.user}: ${r.type} on ${new Date(r.date).toDateString()}\n`;
+  try {
+    let responseBody = `Reservations:\n`
+    for (const r of data.reservations) {
+      responseBody += `${r.user}: ${r.type} on ${new Date(r.date).toDateString()}\n`;
+    }
+    await bot.chat.send(conversationId, {
+      body: responseBody,
+    });
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(conversationId, {
+      body: `Failed to display schedule, see logs for details.`
+    })
   }
-  await bot.chat.send(conversationId, {
-    body: responseBody,
-  });
 }
 
 async function makeReservation(message) {
-  let newReservation = {
-    date: new Date(message.content.text.body.split(' ')[2]),
-    user: message.sender.username,
-    type: message.content.text.body.split(' ')[3]
-  }
-  if (!data.known_types.includes(newReservation.type)) {
+  try {
+    let newReservation = {
+      date: new Date(message.content.text.body.split(' ')[2]),
+      user: message.sender.username,
+      type: message.content.text.body.split(' ')[3]
+    }
+    if (!data.known_types.includes(newReservation.type)) {
+      await bot.chat.send(message.conversationId, {
+        body: 'This is not a reservation type I recognize. Was it a typo?',
+      });
+    }
+    const conflictingReservations = data.reservations.filter(r => 
+      (new Date(r.date).getTime() === newReservation.date.getTime() &&
+        r.type === newReservation.type));
+    if (conflictingReservations.length > 0) {
+      const c = conflictingReservations[0];
+      console.log(`Existing reservation ${JSON.stringify(c)} conflicts with new reservation ${JSON.stringify(newReservation)}`);
+      bot.chat.send(message.conversationId, {
+        body: `This slot is already reserved:
+        ${c.user}: ${c.type} on ${new Date(c.date).toDateString()}.
+        Delete this reservation first if you wish to replace it.`,
+      });
+      return;
+    }
+    data.reservations.push(newReservation);
+    fs.writeFileSync(dataFile, JSON.stringify(data));
     await bot.chat.send(message.conversationId, {
-      body: 'This is not a reservation type I recognize. Was it a typo?',
+      body: 'Reservation made',
     });
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(message.conversationId, {
+      body: `Failed to make reservation, see logs for details.`
+    })
   }
-  const conflictingReservations = data.reservations.filter(r => 
-    (new Date(r.date).getTime() === newReservation.date.getTime() &&
-      r.type === newReservation.type));
-  if (conflictingReservations.length > 0) {
-    const c = conflictingReservations[0];
-    console.log(`Existing reservation ${JSON.stringify(c)} conflicts with new reservation ${JSON.stringify(newReservation)}`);
-    bot.chat.send(message.conversationId, {
-      body: `This slot is already reserved:
-      ${c.user}: ${c.type} on ${new Date(c.date).toDateString()}.
-      Delete this reservation first if you wish to replace it.`,
-    });
-    return;
-  }
-  data.reservations.push(newReservation);
-  fs.writeFileSync(dataFile, JSON.stringify(data));
-  await bot.chat.send(message.conversationId, {
-    body: 'Reservation made',
-  });
 }
 
 async function makeReservationForOther(message) {
-  const user = message.sender.username;
-  if (!data.admins.includes(user)) {
+  try {
+    const user = message.sender.username;
+    if (!data.admins.includes(user)) {
+      await bot.chat.send(message.conversationId, {
+        body: `You do not have permissions to make reservations for other users`,
+      });
+      return;
+    }
+    let newReservation = {
+      date: new Date(message.content.text.body.split(' ')[2]),
+      user: message.content.text.body.split(' ')[4],
+      type: message.content.text.body.split(' ')[3]
+    }
+    if (!data.known_types.includes(newReservation.type)) {
+      await bot.chat.send(message.conversationId, {
+        body: 'This is not a reservation type I recognize. I will make the reservation, please delete it if it was made in error',
+      });
+    }
+    const conflictingReservations = data.reservations.filter(r => 
+      (new Date(r.date).getTime() === newReservation.date.getTime() &&
+        r.type === newReservation.type));
+    if (conflictingReservations.length > 0) {
+      const c = conflictingReservations[0];
+      console.log(`Existing reservation ${JSON.stringify(c)} conflicts with new reservation ${JSON.stringify(newReservation)}`);
+      bot.chat.send(message.conversationId, {
+        body: `This slot is already reserved:
+        ${c.user}: ${c.type} on ${new Date(c.date).toDateString()}.
+        Delete this reservation first if you wish to replace it.`,
+      });
+      return;
+    }
+    data.reservations.push(newReservation);
+    fs.writeFileSync(dataFile, JSON.stringify(data));
     await bot.chat.send(message.conversationId, {
-      body: `You do not have permissions to make reservations for other users`,
+      body: 'Reservation made.',
     });
-    return;
-  }
-  let newReservation = {
-    date: new Date(message.content.text.body.split(' ')[2]),
-    user: message.content.text.body.split(' ')[4],
-    type: message.content.text.body.split(' ')[3]
-  }
-  if (!data.known_types.includes(newReservation.type)) {
+  } catch (err) {
+    console.log(err);
     await bot.chat.send(message.conversationId, {
-      body: 'This is not a reservation type I recognize. I will make the reservation, please delete it if it was made in error',
-    });
+      body: `Failed to make reservation, see logs for details.`
+    })
   }
-  const conflictingReservations = data.reservations.filter(r => 
-    (new Date(r.date).getTime() === newReservation.date.getTime() &&
-      r.type === newReservation.type));
-  if (conflictingReservations.length > 0) {
-    const c = conflictingReservations[0];
-    console.log(`Existing reservation ${JSON.stringify(c)} conflicts with new reservation ${JSON.stringify(newReservation)}`);
-    bot.chat.send(message.conversationId, {
-      body: `This slot is already reserved:
-      ${c.user}: ${c.type} on ${new Date(c.date).toDateString()}.
-      Delete this reservation first if you wish to replace it.`,
-    });
-    return;
-  }
-  data.reservations.push(newReservation);
-  fs.writeFileSync(dataFile, JSON.stringify(data));
-  await bot.chat.send(message.conversationId, {
-    body: 'Reservation made.',
-  });
 }
 
 async function deleteReservation(message) {
-  const date = new Date(message.content.text.body.split(' ')[2]);
-  const user = message.sender.username;
-  const type = message.content.text.body.split(' ')[3];
-  let deletedCount = 0;
-  let remainingReservations = [];
-  for (const r of data.reservations) {
-    if (new Date(r.date).getTime() !== date.getTime() || r.type !== type) {
-      remainingReservations.push(r);
-      continue;
+  try {
+    const date = new Date(message.content.text.body.split(' ')[2]);
+    const user = message.sender.username;
+    const type = message.content.text.body.split(' ')[3];
+    let deletedCount = 0;
+    let remainingReservations = [];
+    for (const r of data.reservations) {
+      if (new Date(r.date).getTime() !== date.getTime() || r.type !== type) {
+        remainingReservations.push(r);
+        continue;
+      }
+      if (r.user === user || data.admins.includes(user)) {
+        deletedCount += 1;
+        continue;
+      } else {
+        remainingReservations.push(r);
+        await bot.chat.send(message.conversationId, {
+          body: `You do not have permissions to delete reservation:
+          ${r.user}: ${r.type} on ${new Date(r.date).toDateString()}.
+          Please contact an admin to delete it.`,
+        });
+      }
     }
-    if (r.user === user || data.admins.includes(user)) {
-      deletedCount += 1;
-      continue;
-    } else {
-      remainingReservations.push(r);
-      await bot.chat.send(message.conversationId, {
-        body: `You do not have permissions to delete reservation:
-        ${r.user}: ${r.type} on ${new Date(r.date).toDateString()}.
-        Please contact an admin to delete it.`,
-      });
-    }
+    data.reservations = remainingReservations;
+    fs.writeFileSync(dataFile, JSON.stringify(data));
+    await bot.chat.send(message.conversationId, {
+      body: `Deleted ${deletedCount} reservations.`,
+    });
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(message.conversationId, {
+      body: `Failed to delete reservation, see logs for details.`
+    })
   }
-  data.reservations = remainingReservations;
-  fs.writeFileSync(dataFile, JSON.stringify(data));
-  await bot.chat.send(message.conversationId, {
-    body: `Deleted ${deletedCount} reservations.`,
-  });
 }
 
 async function makeAdmin(message) {
-  const user = message.sender.username;
-  if (!data.admins.includes(user)) {
+  try {
+    const user = message.sender.username;
+    if (!data.admins.includes(user)) {
+      await bot.chat.send(message.conversationId, {
+        body: `You do not have permissions to grant admin status`,
+      });
+      return;
+    }
+    const toAdd = message.content.text.body.split(' ')[2];
+    data.admins.push(toAdd);
+    fs.writeFileSync(dataFile, JSON.stringify(data));
     await bot.chat.send(message.conversationId, {
-      body: `You do not have permissions to grant admin status`,
+      body: `Made ${toAdd} an admin`,
     });
-    return;
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(message.conversationId, {
+      body: `Failed to make user admin, see logs for details.`
+    })
   }
-  const toAdd = message.content.text.body.split(' ')[2];
-  data.admins.push(toAdd);
-  fs.writeFileSync(dataFile, JSON.stringify(data));
-  await bot.chat.send(message.conversationId, {
-    body: `Made ${toAdd} an admin`,
-  });
 }
 
 async function removeAdmin(message) {
-  const user = message.sender.username;
-  if (!data.admins.includes(user)) {
+  try {
+    const user = message.sender.username;
+    if (!data.admins.includes(user)) {
+      await bot.chat.send(message.conversationId, {
+        body: `You do not have permissions to revoke admin status`,
+      });
+      return;
+    }
+    toRemove = message.content.text.body.split(' ')[2]
+    if (!data.admins.includes(toRemove)) {
+      await bot.chat.send(message.conversationId, {
+        body: `${toRemove} was not an admin`,
+      });
+    } else {
+      data.admins = data.admins.filter(a => a !== toRemove);
+      fs.writeFileSync(dataFile, JSON.stringify(data));
+      await bot.chat.send(message.conversationId, {
+        body: `${toRemove} is no longer an admin`,
+      });
+    }
+  } catch (err) {
+    console.log(err);
     await bot.chat.send(message.conversationId, {
-      body: `You do not have permissions to revoke admin status`,
-    });
-    return;
-  }
-  toRemove = message.content.text.body.split(' ')[2]
-  if (!data.admins.includes(toRemove)) {
-    await bot.chat.send(message.conversationId, {
-      body: `${toRemove} was not an admin`,
-    });
-  } else {
-    data.admins = data.admins.filter(a => a !== toRemove);
-    fs.writeFileSync(dataFile, JSON.stringify(data));
-    await bot.chat.send(message.conversationId, {
-      body: `${toRemove} is no longer an admin`,
-    });
+      body: `Failed to remove admin privileges, see logs for details.`
+    })
   }
 }
 
 async function deleteAll(message) {
-  const user = message.sender.username;
-  if (!data.admins.includes(user)) {
+  try {
+    const user = message.sender.username;
+    if (!data.admins.includes(user)) {
+      await bot.chat.send(message.conversationId, {
+        body: `You do not have permissions to delete all scheduled reservations`,
+      });
+      return;
+    }
+    const numRecords = data.reservations.length;
+    data.reservations = [];
+    fs.writeFileSync(dataFile, JSON.stringify(data));
     await bot.chat.send(message.conversationId, {
-      body: `You do not have permissions to delete all scheduled reservations`,
+      body: `Deleted ${numRecords} records`,
     });
-    return;
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(message.conversationId, {
+      body: `Failed to delete all reservations, see logs for details.`
+    })
   }
-  const numRecords = data.reservations.length;
-  data.reservations = [];
-  fs.writeFileSync(dataFile, JSON.stringify(data));
-  await bot.chat.send(message.conversationId, {
-    body: `Deleted ${numRecords} records`,
-  });
 }
 
 async function killBot(message) {
@@ -260,13 +316,20 @@ async function killBot(message) {
 }
 
 async function listAdmins(conversationId) {
-  let responseBody = `Admins:\n`
-  for (const r of data.admins) {
-    responseBody += `${r}\n`;
+  try {
+    let responseBody = `Admins:\n`
+    for (const r of data.admins) {
+      responseBody += `${r}\n`;
+    }
+    await bot.chat.send(conversationId, {
+      body: responseBody,
+    });
+  } catch (err) {
+    console.log(err);
+    await bot.chat.send(conversationId, {
+      body: `Failed to list admins, see logs for details.`
+    })
   }
-  await bot.chat.send(conversationId, {
-    body: responseBody,
-  });
 }
 
 async function displayHelp(conversationId) {
@@ -323,12 +386,16 @@ async function onMessage(message) {
 }
 
 function purgeOldRecords() {
-  const initialRecords = data.reservations.length;
-  let cutoff = new Date();
-  cutoff.setDate(new Date().getDate() - 7 * pastWeekLimit);
-  data.reservations = data.reservations.filter(r => new Date(r.date).getTime() > cutoff.getTime());
-  fs.writeFileSync(dataFile, JSON.stringify(data));
-  console.log(`Purged ${initialRecords - data.reservations.length} records`);
+  try {
+    const initialRecords = data.reservations.length;
+    let cutoff = new Date();
+    cutoff.setDate(new Date().getDate() - 7 * pastWeekLimit);
+    data.reservations = data.reservations.filter(r => new Date(r.date).getTime() > cutoff.getTime());
+    fs.writeFileSync(dataFile, JSON.stringify(data));
+    console.log(`Purged ${initialRecords - data.reservations.length} records`);
+  } catch (err) {
+    console.log("Failed to purge old records", err);
+  }
 }
 
 async function onError(err) {
